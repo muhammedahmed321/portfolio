906474feb82364801f6c5bce7a8180bb63de1963
Muhammed Ahmed <ahmedkashi857@gmail.com>
2025-07-16 20:11:01 +0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,35 +1,35 @@
+
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+include 'db_config.php';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "ahmedkashi857@gmail.com"; 
+    $subject = "New Contact Form Message";
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    include 'db_config.php';
+    $firstName = $_POST['first_name'];
+    $lastName  = $_POST['last_name'];
+    $email     = $_POST['email'];
+    $phone     = $_POST['phone'];
+    $subjectFromUser = $_POST['subject'];
+    $message   = $_POST['message'];
 
-    if (!isset($_POST['firstName'], $_POST['lastName'], $_POST['email'], $_POST['phone'], $_POST['subject'], $_POST['message'])) {
-        die("Missing form fields");
-    }
+    $body = "Name: $firstName $lastName\n";
+    $body .= "Email: $email\n";
+    $body .= "Phone: $phone\n";
+    $body .= "Subject: $subjectFromUser\n";
+    $body .= "Message:\n$message";
 
-    $firstName = $conn->real_escape_string($_POST['firstName']);
-    $lastName  = $conn->real_escape_string($_POST['lastName']);
-    $email     = $conn->real_escape_string($_POST['email']);
-    $phone     = $conn->real_escape_string($_POST['phone']);
-    $subject   = $conn->real_escape_string($_POST['subject']);
-    $message   = $conn->real_escape_string($_POST['message']);
+    $headers = "From: $email";
 
-    $sql = "INSERT INTO contact_messages (first_name, last_name, email, phone, subject, message)
-            VALUES ('$firstName', '$lastName', '$email', '$phone', '$subject', '$message')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "<script>alert('Message sent successfully!'); window.location.href='index.html';</script>";
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
     } else {
-        die("DB Error: " . $conn->error);
+        echo "Error: Message could not be sent.";
     }
-
-    $conn->close();
 } else {
-    die("Invalid request method: " . $_SERVER["REQUEST_METHOD"]);
+    echo "Not allowed!";
 }
 ?>
+
 
 
 
